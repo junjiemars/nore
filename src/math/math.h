1f402fc04ec3bdfe1f77359d79c04be130e47296
南山竹 <junjiemars@gmail.com>
2016-09-06 17:17:31 +0800

    stick's makefile

--- src/math/math.h
+++ src/math/math.h
@@ -4,25 +4,5 @@
 #include <nm_auto_config.h>
 #include <nm_auto_headers.h>
 
-#if (MSYS_NT)
-
-#define GEO_API __declspec(dllimport) __stdcall
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-double GEO_API area_of_rect(double, double);
-
-#ifdef __cplusplus
-}
-#endif
-
-#else
-
-double area_of_rect(double, double);
-
-#endif // MSYS_NT
-
 
 #endif // _MATH_H_
