b47b537b4b319aad973c4c1d74a427101c1cc26d
南山竹 <junjiemars@gmail.com>
2016-09-08 02:14:36 +0800

    refine bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,23 +13,39 @@ BEGIN=`date +%s`
 echo "configure Nore on $PLATFORM ..."
 echo
 
+echo -n "checking bash environment ... "
 if [ ! -f $HOME/.bash_paths -o ! -f $HOME/.bash_vars ]; then
+	echo "no found"
+	echo 
 	curl -sqLo /tmp/setup-bash.sh ${GITHUB_R}/kit/master/ul/setup-bash.sh && \
 		. /tmp/setup-bash.sh
 	. $HOME/.bashrc
+else
+	echo "found"
 fi
 
-# setup gmake for windows
-case ${PLATFORM} in
-  MSYS_NT*)
-		HAS_GMAKE=1 bash <(curl ${GITHUB_R}/kit/master/win/install-win-kits.sh)
-		;;
-  *)
-    ;;
-esac
+echo -n "checking make ... "
+if `make -v &>/dev/null`; then
+	echo "found"
+else
+	echo "no found"
+	case ${PLATFORM} in
+	  MSYS_NT*)
+			HAS_GMAKE=1 bash <(curl ${GITHUB_R}/kit/master/win/install-win-kits.sh)
+			;;
+	  *)
+	    ;;
+	esac
+fi
 
-# setup Nore env
-if [ ! -x nore/auto/configue ]; then
+echo -n "checking Nore ... "
+if [ -x nore/auto/configure ]; then
+	echo "found"
+	echo -n "refreshing Nore ... "
+	cd nore && git reset --hard && git pull origin master
+	echo "done"
+else
+	echo "no found"
 	git clone --depth=1 --branch=master ${GITHUB_H}/nore.git
 fi
 
