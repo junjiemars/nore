601a38dee3a45c602e3f9959c23556f4ad4ffcc7
南山竹 <junjiemars@gmail.com>
2017-09-24 10:05:02 +0800

    travis: osx, linux

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+make clean
+
+case "$TRAVIS_OS_NAME" in
+  osx)
+    ./configure --has-hi
+    make test
+  ;;
+
+  linux|*)
+    CC=gcc ./configure --has-hi
+    make test
+
+    make clean
+    CC=clang ./configure --has-hi
+    make test
+  ;;
+esac
 
