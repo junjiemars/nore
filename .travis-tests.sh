5fadb2087eb0fcbb587be6643c5bfad866668b22
南山竹 <junjiemars@gmail.com>
2018-05-24 23:45:37 +0800

    travis: format

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -2,11 +2,11 @@
 
 [ -d "$TRAVIS_BUILD_DIR" ] && cd "$TRAVIS_BUILD_DIR"
 
-echo "============"
+echo "------------"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 echo "./configure where"
 echo "`./configure where`"
-echo "============"
+echo "------------"
 
 CC=
 case "$TRAVIS_OS_NAME" in
@@ -15,23 +15,29 @@ case "$TRAVIS_OS_NAME" in
   *) : ;;
 esac
 
+echo_test_what() {
+	echo "------------"
+	echo "# testing $1 option ..."
+	echo "------------"
+}
+
 # basic testing
 CC=$CC ./configure --new
 CC=$CC ./configure
 make clean test
 
 # --with-optimize testing
-echo "============"
+echo_test_what "--with-optimize="
 CC=$CC ./configure --with-optimize=YES
 make clean test
 
 # --with-std=c11 testing
-echo "============"
+echo_test_what "--with-std=c11"
 CC=$CC ./configure --with-std=c11
 make clean
 
 # --without-* testing
-echo "============"
+echo_test_what "--without-*="
 CC=$CC ./configure \
 	--without-symbol \
 	--without-debug \
@@ -41,6 +47,6 @@ CC=$CC ./configure \
 make clean test
 
 # --src-dir --out-dir testing
-echo "============"
+echo_test_what "--src/out-dir="
 CC=$CC ./configure --src-dir=src --out-dir=build
 make clean test
