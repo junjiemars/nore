4c9e20f71870bbc4c9242a020d1f35b13bc1d98a
南山竹 <junjiemars@gmail.com>
2018-05-24 23:38:37 +0800

    travis: configure options testing

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -10,20 +10,37 @@ echo "============"
 
 CC=
 case "$TRAVIS_OS_NAME" in
-  osx)
-    CC=clang
-  ;;
-
-  linux)
-    CC=gcc
-  ;;
-
-  *)
-		:
-	;;
+  osx) CC=clang ;;
+  linux) CC=gcc ;;
+  *) : ;;
 esac
 
+# basic testing
 CC=$CC ./configure --new
 CC=$CC ./configure
 make clean test
 
+# --with-optimize testing
+echo "============"
+CC=$CC ./configure --with-optimize=YES
+make clean test
+
+# --with-std=c11 testing
+echo "============"
+CC=$CC ./configure --with-std=c11
+make clean
+
+# --without-* testing
+echo "============"
+CC=$CC ./configure \
+	--without-symbol \
+	--without-debug \
+	--without-error \
+	--without-warn \
+	--with-verbose
+make clean test
+
+# --src-dir --out-dir testing
+echo "============"
+CC=$CC ./configure --src-dir=src --out-dir=build
+make clean test
