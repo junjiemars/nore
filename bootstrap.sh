21eb7a93fb63cc07219765e190642e553fc648d5
南山竹 <junjiemars@gmail.com>
2018-07-09 13:13:56 +0800

    clean

--- bootstrap.sh
+++ bootstrap.sh
@@ -507,7 +507,6 @@ else
  exit_checking $? $BEGIN
 fi
 
-echo "!!! arg0=$0 NORE_BRANCH=$NORE_BRANCH"
 echo -n " + generating configure ... "
 echo_ok_or_failed `cat_configure ; echo $?`
 exit_checking $? $BEGIN
