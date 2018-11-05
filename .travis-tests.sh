ad1372dd7c625a8d5f4da1ac4e8cbf2f36b92331
南山竹 <junjiemars@gmail.com>
2018-11-05 14:12:24 +0800

    refine .travis-tests.sh: run on local env

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -1,56 +1,86 @@
 #!/bin/bash
 
-[ -d "$TRAVIS_BUILD_DIR" ] && cd "$TRAVIS_BUILD_DIR"
 
-echo "------------"
-echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
-echo "./configure where"
-echo "`./configure where`"
-echo "------------"
+using_cc() {
+	if [ -z "$1" ]; then
+		case "$TRAVIS_OS_NAME" in
+			osx)     CC=clang ;;
+			linux)   CC=gcc   ;;
+			*)       :        ;;
+		esac
+	elif `command -v $1`; then
+		CC="$1"
+	fi
+}
 
-CC=
-case "$TRAVIS_OS_NAME" in
-  osx) CC=clang ;;
-  linux) CC=gcc ;;
-  *) : ;;
-esac
 
-echo_test_what() {
+make_ci_env() {
+  if [ ".true" != "$TRAVIS" ]; then
+  	TRAVIS_BUILD_DIR="`dirname ${BASH_SOURCE[0]}`"
+  fi
+
+  nore_ci_dir="${TRAVIS_BUILD_DIR%/}/ci"
+  if [ -d "$nore_ci_dir" ]; then
+		rm -r "${nore_ci_dir}"
+	fi
+	mkdir -p "$nore_ci_dir"
+  cd "$nore_ci_dir"
+
+  echo "------------"
+  echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
+  echo "------------"
+  echo "${TRAVIS_BUILD_DIR%/}/bootstrap.sh"
+  echo "`${TRAVIS_BUILD_DIR%/}/bootstrap.sh`"
+  echo "------------"
+  echo "${nore_ci_dir}/configure where"
+  echo "`${nore_ci_dir}/configure where`"
+  echo "------------"
+
+	using_cc
+}
+
+echo_ci_what() {
 	echo "------------"
-	echo "# testing $1 option ..."
+	echo "# CI: $1 option ..."
 	echo "------------"
 }
 
 
-CC=$CC ./configure --new
-CC=$CC ./configure
-make clean test
+ci_nore_options() {
+	make_ci_test
+
+	CC=$CC ./configure --new
+	CC=$CC ./configure
+	make clean test
+
+	echo_ci_what "--with-optimize="
+	CC=$CC ./configure --with-optimize=YES
+	make clean test
+
+	echo_ci_what "--with-std=c11"
+	CC=$CC ./configure --with-std=c11
+	make clean test
+
+	echo_ci_what "--without-*="
+	CC=$CC ./configure \
+		--without-symbol \
+		--without-debug \
+		--without-error
+	make clean test
+
+	echo_ci_what "--with-warn, --with-verbose="
+	CC=$CC ./configure \
+		--with-warn=NO \
+		--with-verbose
+	make clean test
+
+	make_ci_env
+	echo_ci_what "--src-dir, --out-dir="
+	CC=$CC ./configure --src-dir=src --out-dir=out --new
+	make clean test
+}
 
 
-echo_test_what "--with-optimize="
-CC=$CC ./configure --with-optimize=YES
-make clean test
+ci_nore_options
 
-
-echo_test_what "--with-std=c11"
-CC=$CC ./configure --with-std=c11
-make clean
-
-
-echo_test_what "--without-*="
-CC=$CC ./configure \
-	--without-symbol \
-	--without-debug \
-	--without-error
-make clean test
-
-
-echo_test_what "--with-warn/verbose="
-CC=$CC ./configure \
-	--with-warn=NO \
-	--with-verbose
-
-
-echo_test_what "--src/out-dir="
-CC=$CC ./configure --src-dir=src --out-dir=build
-make clean test
+[ -d "${nore_ci_dir}" ] && rm -r "${nore_ci_dir}"
