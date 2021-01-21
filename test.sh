2e5f2102eb99d6064d081932b176776ae5698bf4
南山竹 <junjiemars@gmail.com>
2021-01-21 21:47:11 +0800

    test: auto

--- test.sh
+++ test.sh
@@ -78,7 +78,7 @@ END
       exit 1
     fi
     chmod u+x ${msvc_bat}
-    ./${msvc_bat} "${_WIN_ENV_}" "./configure ${_TRACE_} $@"
+    ./${msvc_bat} "${_WIN_ENV_}" "configure ${_TRACE_} $@"
   fi
 }
 
@@ -293,7 +293,7 @@ test_nore_auto_check () {
 # test
 env_ci_build
 # test_nore_where_command
-# test_nore_new_option
+test_nore_new_option
 # test_nore_symbol_option
 # test_nore_optimize_option
 # test_nore_std_option
